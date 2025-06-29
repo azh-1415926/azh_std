043672c50a9323e39e79a346be017fedcfc8cb63
azh-1415926 <3200417937@qq.com>
2025-06-29 19:09:48 +0800

    增加版本号

--- include/azh/version.hpp
+++ include/azh/version.hpp
@@ -6,3 +6,5 @@
 #define AZH_VERSION_MAJOR 0
 #define AZH_VERSION_MINOR 1
 #define AZH_VERSION_PATCH 0
+
+#define AZH_VERSION std::string()+std::to_string(AZH_VERSION_MAJOR)+"."+std::to_string(AZH_VERSION_MINOR)+"."+std::to_string(AZH_VERSION_PATCH)
